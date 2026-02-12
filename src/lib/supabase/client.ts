ba95bb899b10582dcbea4a9c475ca0449778c5f7
Jatin <jatin02k@users.noreply.github.com>
2026-02-13 00:02:54 +0530

    google login working fine

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,26 +1,8 @@
 import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
-  let cookieDomain: string | undefined = undefined;
-
-  if (typeof window!=='undefined' && process.env.NODE_ENV === 'production') { 
-    const root = process.env.NEXT_PUBLIC_APP_URL;
-    if(root && window.location.hostname.endsWith(root)){
-      cookieDomain = `.${root}`;
-    }
-  }
-
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookieOptions:{
-        domain: cookieDomain,
-        path: '/',
-        sameSite: 'lax',
-        secure:process.env.NODE_ENV === 'production',
-        httpOnly: true,
-      }
-    }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
