afe1ae9f9985a332c1b7559bd3366cc2c2b07d67
Jatin <183578648+jatin02k@users.noreply.github.com.>
2026-02-23 16:54:55 +0530

    new dep

--- src/components/dashboard/dashboardClient.tsx
+++ src/components/dashboard/dashboardClient.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useState } from 'react'
-
 import { User } from '@supabase/supabase-js'
 import { Flame, LogOut } from 'lucide-react'
 import { createClient } from '@/lib/supabase/client'
